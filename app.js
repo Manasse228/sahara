5380e33a179f37b5443d46e778ada6796c2742ca
Manassé <Manass>
2019-07-11 15:49:43 +0200

    fix connection error about async

--- app.js
+++ app.js
@@ -7,12 +7,14 @@ const CoinGecko             = require('coingecko-api');
 const CoinCtrl              = require('./routes/CoinCtrl');
 const CoinDetailCtrl        = require('./routes/CoinDetailCtrl');
 const CoinDetail            = require('./models/CoinDetails');
+const locks                 = require('locks');
 
 
+const mutex         = locks.createMutex();
 const CoinGeckoClient = new CoinGecko();
 const app = express();
 
-const initiateCOinDB = async() => {
+const initiateCoinDB = async() => {
     const data = await CoinGeckoClient.ping();
     if (data.data) {
         let coinList = await CoinGeckoClient.coins.list();
@@ -24,27 +26,41 @@ const initiateCOinDB = async() => {
 };
 
 
-initiateCOinDB().then( async () => {
+initiateCoinDB().then( async () => {
     let coinList = await CoinGeckoClient.coins.list();
     coinList = coinList.data;
     CoinCtrl.setDeleteCoin(coinList);
 } );
 
-const fetchCoinDetail = async () => {
-    CoinCtrl.getAllCoins().then( (result) => {
+const fetchCoinDetail =  () => {
+    CoinCtrl.getAllCoins().then( async (result) => {
         for (let i=0; i< result.length; i++) {
-            CoinDetail.getCoinDetailById(result[i]._id, async (err, _result) => {
+            await CoinDetail.getCoinDetailById(result[i]._id, async (err, _result) => {
                 if (!_result) {
-                    let data = await CoinGeckoClient.coins.fetch(result[i]._id);
-                    console.log('i ', i);
-                    await CoinDetailCtrl.register(result[i]._id, data.data);
+                    try {
+
+                        mutex.lock( async () => {
+                            await getCoinDetailsSlowly(result[i]._id).then();
+                            mutex.unlock();
+                        });
+
+                    } catch (e) {
+                       console.log('error ', e)
+                    }
+
                 }
             })
+
         }
     })
 } ;
 
-fetchCoinDetail().then();
+const getCoinDetailsSlowly = async (id) => {
+    let data = await CoinGeckoClient.coins.fetch(id);
+    await CoinDetailCtrl.register(id, data.data);
+};
+
+fetchCoinDetail();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
